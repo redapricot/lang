56e2c6994f08fab4d811a5e05994aa016bc7f347
redapricot <12511276+redapricot@users.noreply.github.com>
2022-10-31 14:56:48 +0800

    deploy

---
+++ assets/index.1da0ce6d.js
@@ -0,0 +1,1 @@
+import{_ as n,c as a,o,a as s}from"./index.34a1416d.js";const t={},r=[s("h1",null,"404 not find",-1)];const d=n(t,[["render",function(n,s){return o(),a("div",null,r)}]]);export{d as default};
